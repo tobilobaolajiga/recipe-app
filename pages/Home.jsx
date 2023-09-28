3efb45bf3e47e0b2c200ab8d19b5cd7d703005c9
tobilobaolajiga <tobilobaolajiga@gmail.com>
2023-09-28 12:15:20 +0100

    first commit

---
+++ pages/Home.jsx
@@ -0,0 +1,17 @@
+import Veggie from "../components/Veggie"
+import Popular from "../components/popular"
+import {motion, AnimatePresence} from 'framer-motion'
+
+const Home = () => {
+  return (
+      <motion.div animate={{ opacity: 1 }}
+      initial={{ opacity: 0 }}
+      exit={{ opacity: 0 }}
+        transition = {{duration:0.5}}>
+          <Veggie />
+          <Popular/>
+    </motion.div>
+  )
+}
+
+export default Home
---
+++ pages/Pages.jsx
@@ -0,0 +1,26 @@
+import React from "react"
+import Home from "./Home"
+import Cuisine from "./Cuisine"
+import Searched from './Searched'
+import Recipe from "./Recipe"
+import { AnimatePresence } from "framer-motion"
+import { Route, Routes, useLocation } from 'react-router-dom'
+
+
+const Pages = () => {
+  const location = useLocation()
+  return (
+        <AnimatePresence mode='wait'>
+        <Routes location ={location} key={location.pathname}>
+          <Route path='/' element={<Home />} />
+      <Route path='/cuisine/:type' element={<Cuisine />} />
+      <Route path='/searched/:search' element={<Searched />} />
+      <Route path='/recipe/:name' element={<Recipe />} />
+      </Routes>
+      </AnimatePresence>
+        
+     
+  )
+}
+
+export default Pages
